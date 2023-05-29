bf308ef6aa33dc039367dd82176a8e868f9f2baf
Joseph Omotade <josephomotade0@gmail.com>
2023-05-29 23:02:44 +0100

    commit

--- src/components/canvas/BallCanvas.tsx
+++ src/components/canvas/BallCanvas.tsx
@@ -8,26 +8,26 @@ const Ball = (props: any) => {
   const [decal] = useTexture([props.imgUrl.src])
 console.log(props.imgUrl.src)
   return (
-   <Float speed={1.5} rotationIntensity={1} floatIntensity={2} >
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
     <ambientLight intensity={0.25} />
     <directionalLight position={[0, 0, 0.05]} />
-    <mesh castShadow receiveShadow scale={2.4}>
+    <mesh castShadow receiveShadow scale={2.3}>
       <icosahedronGeometry args={[1, 1]} />
       <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-           <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
+        color='#fff8eb'
+        polygonOffset
+        polygonOffsetFactor={-5}
+        flatShading
+      />
+      <Decal
+        position={[0, 0, 1]}
+        rotation={[2 * Math.PI, 0, 6.25]}
+        scale={1}
+        map={decal}
+        flatShading
+      />
     </mesh>
-   </Float>
+  </Float>
   );
 };
 
