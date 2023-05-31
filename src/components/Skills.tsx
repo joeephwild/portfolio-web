743ce2897f410ab7035a1b16b3d894a2bb5c7a0d
Joseph Omotade <josephomotade0@gmail.com>
2023-05-31 23:39:32 +0100

    commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -20,83 +20,82 @@ type Props = {
 };
 
 const ProjectCard = ({
-    index,
-    name,
-    description,
-    tags,
-    image,
-    source_code_link,
-    live,
-  }: Props) => {
-    return (
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  live,
+}: Props) => {
+  return (
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary bg-blue-60 p-5 rounded-2xl sm:w-[360px] w-full"
+    >
       <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-        <Tilt
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-        >
-          <div className="relative w-full h-[230px]">
-            <Image
-              src={image}
-              alt="project_image"
-              className="w-full h-full object-cover rounded-2xl"
-            />
-  
-            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-              <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className="bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              >
-                <Image
-                  src={github}
-                  alt="source code"
-                  className="w-1/2 h-1/2 object-contain"
-                />
-              </div>
+        <div className="relative w-full h-[230px]">
+          <Image
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <Image
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
             </div>
           </div>
-  
-          <div className="mt-5">
-            <div className="flex items-center justify-between">
-              <h3 className="text-white font-bold text-[24px]">{name}</h3>
-              <div
-                onClick={() => window.open(live, "_blank")}
-                className="cursor-pointer"
-              >
-                <FaArrowRight size={25} />
-              </div>
+        </div>
+
+        <div className="mt-5">
+          <div className="flex items-center justify-between">
+            <h3 className="text-white font-bold text-[24px]">{name}</h3>
+            <div
+              onClick={() => window.open(live, "_blank")}
+              className="cursor-pointer"
+            >
+              <FaArrowRight size={25} />
             </div>
-  
-            <p className="mt-2 text-secondary text-[14px]">{description}</p>
           </div>
-  
-          <div className="mt-4 flex flex-wrap gap-2">
-            {tags.map((tag: any) => (
-              <p
-                key={`${name}-${tag.name}`}
-                className={`text-[14px] ${tag.color}`}
-              >
-                #{tag.name}
-              </p>
-            ))}
-          </div>
-        </Tilt>
+
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag: any) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
       </motion.div>
-    );
-  };
+    </Tilt>
+  );
+};
 
 const Skills = () => {
   return (
     <div>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
-
-      <motion.p
+      <div className="w-full flex">
+        <motion.p
           variants={fadeIn("left", "spring", 0.1, 1)}
           className="mt-3 text-[#fff] xsm:mx-2 xsm:text-left text-[17px] tracking-tight font-Nunito-Black xsm:text-sm font-light max-w-3xl leading-[30px]"
         >
@@ -108,8 +107,8 @@ const Skills = () => {
           works where i only worked as smart contract developer for projects
           during hackathons.
         </motion.p>
-
-        <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:grid gap-12">
+      </div>
+      <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:grid md:grid gap-12">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
@@ -118,4 +117,4 @@ const Skills = () => {
   );
 };
 
-export default Skills;
+export default StarWrapper(Skills, "work");
